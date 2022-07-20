27e3cc9b6fb4ae6a11a007034e2c76c801f2e10e
Donildo <109067041+Donildo@users.noreply.github.com>
2022-07-20 10:40:12 -0400

    Add files via upload

---
+++ desafio2.ts
@@ -0,0 +1,29 @@
+interface pessoa{
+    nome: string;
+    idade: number;
+    profissao: string;
+}
+
+let pessoa1: Humano ={
+    nome: 'Maria'
+    idade: 29;
+    profissao: 'atriz'
+};
+
+let pessoa2: Humano ={
+    nome: 'Roberto'
+    idade: 19;
+    profissao: 'padeiro'
+};
+
+let pessoa3: Humano ={
+    nome: 'Laura'
+    idade: 32;
+    profissao: 'atriz'
+};
+
+let pessoa4: Humano ={
+    nome: 'Carlos'
+    idade: 19;
+    profissao: 'padeiro'
+};
